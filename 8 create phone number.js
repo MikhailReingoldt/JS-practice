8cc953d0dd4dae4dc021f16a275e6d56d4ac7539
MikhailReingoldt <vonreinholdt543@gmail.com>
2023-03-10 15:01:51 +0300

    task solved

---
+++ 8 create phone number.js
@@ -0,0 +1,8 @@
+let phoneNumber = [9, 5, 1, 6, 8, 8, 6, 8, 8, 9];
+function createPhoneNumber(phoneNumber) {
+  let areaCode = phoneNumber.slice(0, 3).join("");
+  let prefix = phoneNumber.slice(3, 6).join("");
+  let lineNumber = phoneNumber.slice(6, 9).join("");
+  return `(${areaCode}) ${prefix}-${lineNumber}`;
+}
+console.log(createPhoneNumber(phoneNumber));
