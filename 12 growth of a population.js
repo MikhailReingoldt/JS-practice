6ec52654fd5dfa3e1b9799be950e81ec13410b75
MikhailReingoldt <vonreinholdt543@gmail.com>
2023-03-13 15:12:31 +0300

    task solved

--- 12 growth of a population.js
+++ 12 growth of a population.js
@@ -2,8 +2,8 @@ let p0 = 1000;
 let increasePercentage = 0.02;
 let additionalInhabitants = 50;
 let targetPopulation = 1200;
-let years = 0;
 
+let years = 0; /* задаём начальное значение счётчика */
 while (p0 < targetPopulation) {
   p0 = p0 + p0 * increasePercentage + additionalInhabitants;
   years++;
---
+++ 13 perfect number verifier.js
@@ -0,0 +1,15 @@
+result = 0;
+function sum(number) {
+  for (i = 1; i <= number / 2; i++) {
+    if (number % i == 0) {
+      result = result + i;
+    }
+  }
+  if (result == number) {
+    return `${number} is perfect number`;
+  } else {
+    return `${number} is not perfect number`;
+  }
+}
+
+console.log(sum(25));
