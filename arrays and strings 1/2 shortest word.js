3353987ebee55af7daf3031e09596f514a5e4a1c
MikhailReingoldt <vonreinholdt543@gmail.com>
2023-03-15 11:25:22 +0300

    task solved

--- arrays and strings 1/2 shortest word.js
+++ arrays and strings 1/2 shortest word.js
@@ -1,6 +1,8 @@
 let string = "short string for test fds f";
 let array = string.split(" ");
-let wordsLength = array.map((item) => item.length);
+let wordsLength = array.map((word) => word.length);
+/* в данном случае мы word можем заменить на любое другое обозначение */
 let shortestWord = Math.min(...wordsLength);
+/* оператор ... распространяет массив wordsLength на отдельные значения, которые затем передаются в качестве аргументов функции Math.min. */
 
 console.log(shortestWord);
