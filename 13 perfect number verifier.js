56c59d127ea34ad33c556652a52780cdda2bf113
MikhailReingoldt <vonreinholdt543@gmail.com>
2023-03-15 15:38:12 +0300

    task solved

--- 13 perfect number verifier.js
+++ 13 perfect number verifier.js
@@ -12,4 +12,4 @@ function sum(number) {
   }
 }
 
-console.log(sum(25));
+console.log(sum(28));
--- 8 create phone number.js
+++ 8 create phone number.js
@@ -1,6 +1,7 @@
 let phoneNumber = [9, 5, 1, 6, 8, 8, 6, 8, 8, 9];
 function createPhoneNumber(phoneNumber) {
   let areaCode = phoneNumber.slice(0, 3).join("");
+  /* .join используется для того, чтобы массив преобразовать в строку */
   let prefix = phoneNumber.slice(3, 6).join("");
   let lineNumber = phoneNumber.slice(6, 9).join("");
   return `(${areaCode}) ${prefix}-${lineNumber}`;
